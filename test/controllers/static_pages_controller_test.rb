97266ba13dc1a5c7a7945eb496fd91552a067ade
Kusum Ijari <kusumijari@gmail.com>
2015-11-26 13:12:49 +0530

    Removed 'home' from title for home page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,7 +8,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
@@ -23,10 +23,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | #{@base_title}"
   end
 
-  # test "should get contact" do
-  #   get :contact
-  #   assert_response :success
-  #   assert_select "title", "Contact | #{@base_title}"
-  # end
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
 
 end
